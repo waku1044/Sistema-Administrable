656b1bbed1419165183d150b11f250a585659bcb
Walter Gimenez <75543829+waku1044@users.noreply.github.com>
2024-04-03 17:54:12 -0300

    Se elimino codigo js

--- index.js
+++
@@ -1,6 +0,0 @@
-const btn_menu = document.querySelector("[data-btn-menu]");
-const submenu = document.querySelector(".submenu");
-
-btn_menu.addEventListener("click", () => {
-    submenu.hidden = !submenu.hidden;
-});
