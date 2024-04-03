c811e31611648c6ea165dab865d9d85863d6f42a
Walter Gimenez <75543829+waku1044@users.noreply.github.com>
2024-04-03 17:52:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-const btn_menu = document.querySelector("[data-btn-menu]");
-const submenu = document.querySelector(".submenu");
 
-btn_menu.addEventListener("click", () => {
-    submenu.hidden = !submenu.hidden;
-});
